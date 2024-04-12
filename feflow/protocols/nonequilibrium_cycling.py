8c69b31364e2751c0b30ad12c15a2e822434c5d9
Ian Kenney <ianmichaelkenney@gmail.com>
2024-04-12 18:53:35 +0000

    Updated ValueError messages when extending
    * When `mapping` is `None`, use the mapping provided by the
      ProtocolDAGResult

--- feflow/protocols/nonequilibrium_cycling.py
+++ feflow/protocols/nonequilibrium_cycling.py
@@ -974,13 +974,16 @@ class NonEquilibriumCyclingProtocol(Protocol):
             original_mapping = setup.inputs["mapping"]
 
             if original_state_a != stateA.key:
-                raise ValueError()
+                raise ValueError("'stateA' key is not the same as the key provided by the 'extends' ProtocolDAGResult.")
 
             if original_state_b != stateB.key:
-                raise ValueError()
+                raise ValueError("'stateB' key is not the same as the key provided by the 'extends' ProtocolDAGResult.")
 
-            if original_mapping != mapping:
-                raise ValueError()
+            if mapping is not None:
+                if original_mapping != mapping:
+                    raise ValueError("'mapping' is not consistent with the mapping provided by the 'extnds' ProtocolDAGResult.")
+            else:
+                mapping = original_mapping
 
             systems = {}
             states = {}
