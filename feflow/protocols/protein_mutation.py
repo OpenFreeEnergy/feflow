3e363bcdff41e7b919150dc3b21e68a676b88873
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2024-08-26 18:47:40 -0400

    WIP -- helper utilities. Test function template.

--- feflow/protocols/protein_mutation.py
+++ feflow/protocols/protein_mutation.py
@@ -2,14 +2,51 @@
 Implementation of protein mutation protocol based on Nonequilibrium Cycling, using OpenMM as
 MD engine.
 """
+from pathlib import Path
 from typing import Dict, Any
 
 from gufe import ChemicalSystem, Context,  ProtocolUnit, AtomMapping
 
 
+def mutate_with_pdbfixer(filename: str | Path, mutation_spec: str, chain_id: str = "A", output_pdb=None):
+    """
+    Takes a pdb path and applies mutation using pdbfixer.
+
+    Parameters
+    ----------
+    filename
+    mutation_spec
+    chain_id
+    output_pdb
+
+    Returns
+    -------
+
+    """
+    import pdbfixer
+    from openmm.app import PDBFile
+    fixer = pdbfixer.PDBFixer(filename=str(filename))
+    # make sure your structure is complete
+    fixer.findMissingResidues()
+    fixer.findMissingAtoms()
+    if bool(fixer.missingAtoms) or bool(fixer.missingResidues):
+        raise ValueError(f"Your structure has the following missing elements. Missing residues: "
+                         f"{fixer.missingResidues}; missing atoms: {fixer.missingAtoms}. Please fix.")
+    fixer.applyMutations(mutations=[mutation_spec], chain_id=chain_id)
+    omm_topology = fixer.topology
+    omm_positions = fixer.positions
+
+    # Write pdb file if specified
+    if output_pdb:
+        with open(output_pdb, "w") as out_file:
+            PDBFile.writeFile(omm_topology, omm_positions, out_file)
+
+    return omm_topology, omm_positions
+
+
 class SetupUnit(ProtocolUnit):
     """
-    Initial unit of the protocol. Creates a hybrid topology for the protein mutation and
+    Initial un      it of the protocol. Creates a hybrid topology for the protein mutation and
     generates the OpenMM.
     """
     def _execute(ctx: Context, *, state_a: ChemicalSystem, state_b: ChemicalSystem, mapping: AtomMapping, **inputs) -> Dict[str, Any]:
--- feflow/tests/test_protein_mutation.py
+++ feflow/tests/test_protein_mutation.py
@@ -1,6 +1,42 @@
 """Test related to the protein mutation protocol and utility functions"""
 
+from importlib.resources import files
+
 
 class TestProtocolMutation:
     def test_protocol(self):
         return NotImplementedError
+
+
+def test_mutation_utility():
+    """
+    Test utility function that creates two chemical systems for the protein mutation protocol.
+
+    It takes an Alanine dipeptide and mutate it to a Proline, generating the corresponding chemical
+    systems and mapping objects.
+
+    Returns
+    -------
+
+    """
+    from gufe import ProteinComponent
+
+    pdb_path = files("feflow.tests.data").joinpath("ALA_capped.pdb")
+    # Want to mutate ALA in ACE-ALA-NME seq.
+    orig_resname = "ALA"
+    target_resname = "PRO"
+    resid = 2
+    chain_id = 1
+    mutation_spec = f"{orig_resname}{resid}{target_resname}"
+
+    # Generate components for mutation
+    comp_a, comp_b = some_utility_function(pdb_path=pdb_path, mutation_spec=mutation_spec, chain_id=chain_id)
+    omm_top_a = comp_a.to_openmm_topology()
+    omm_top_a = comp_b.to_openmm_topology()
+
+    assert omm_top_a == state_b, "Define message here!"
+
+    # Create mapping object
+    mapping_obj = some_mapping_function(state_a, state_b)
+
+
