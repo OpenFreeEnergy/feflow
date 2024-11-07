ffc3aaddc8f86a2ab119cfd0d9617143c77bd65e
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2024-11-07 16:14:26 -0500

    Better name for custom error. Add comment/todo.

--- feflow/utils/exceptions.py
+++ feflow/utils/exceptions.py
@@ -1,11 +1,12 @@
 """Module with custom exceptions that can be useful for including in protocols"""
 
-
+# TODO: We should implement the base class in gufe.
+#  Issue https://github.com/OpenFreeEnergy/gufe/issues/385
 class ProtocolError(Exception):
     """Base exception for OpenFE protocols"""
 
 
-class MethodConstraintError(ProtocolError):
+class MethodLimitationtError(ProtocolError):
     """Custom exception to be raised when a fundamental limitation in the methodology is
     that does not support."""
 
