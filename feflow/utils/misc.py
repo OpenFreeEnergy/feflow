8fd3877043e1200e80677763c9da406b6a07d391
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2025-01-29 19:01:40 -0500

    Adding miscellaneous utility functions

--- feflow/utils/misc.py
+++ feflow/utils/misc.py
@@ -101,3 +101,112 @@ def register_ff_parameters_template(system_generator, charge_settings, openff_mo
         system_generator.create_system(
             mol.to_topology().to_openmm(), molecules=[mol]
         )
+
+
+# TODO: Maybe this needs to be in another module with a more telling name. Also, overkill?
+def generate_omm_top_from_component(comp: gufe.SmallMoleculeComponent | gufe.ProteinComponent):
+    """
+    Generate an OpenMM `Topology` object from a given `SmallMoleculeComponent` or
+    `ProteinComponent`.
+
+    This function attempts to generate an OpenMM `Topology` object from the provided
+    component. It handles both components that directly support conversion to an
+    OpenMM topology (`to_openmm_topology`) and those that require an intermediate
+    conversion through OpenFF (`to_openff().to_topology().to_openmm()`).
+
+    Parameters
+    ----------
+    comp : gufe.SmallMoleculeComponent | gufe.ProteinComponent
+        The component to be converted into an OpenMM `Topology`. Supported components include
+        `SmallMoleculeComponent` and `ProteinComponent`.
+
+    Returns
+    -------
+    openmm.app.Topology
+        The corresponding OpenMM `Topology` object for the given component.
+
+    Raises
+    ------
+    AttributeError
+        If the component does not support the necessary conversion methods.
+    """
+
+    try:
+        topology = comp.to_openmm_topology()
+    except AttributeError:
+        topology = comp.to_openff().to_topology().to_openmm()
+
+    return topology
+
+
+def get_positions_from_component(comp: gufe.SmallMoleculeComponent | gufe.ProteinComponent):
+    """
+    Retrieve the positions of atoms in a component as an OpenMM Quantity.
+
+    This function tries to get the atomic positions from the component. If the component has
+    a method `to_openmm_positions()`, it uses that to fetch the positions. If the component
+    doesn't have that method (i.e., it doesn't support OpenMM directly), it falls back to
+    extracting the positions from the OpenFF conformers and takes the first conformer.
+
+    Parameters
+    ----------
+    comp : gufe.SmallMoleculeComponent | gufe.ProteinComponent
+        The component (small molecule or protein) for which atomic positions are required.
+
+    Returns
+    -------
+    openmm.Quantity
+        A quantity representing the atomic positions in OpenMM format.
+
+    Raises
+    ------
+    AttributeError
+        If neither `to_openmm_positions()` nor OpenFF conformers are available.
+    """
+    # NOTE: Could potentially be done with rdkit if we want to rely solely on it, something like:
+    # # Retrieve the first conformer (if multiple conformers exist)
+    # mol = comp.to_rdkit()
+    # conformer = mol.GetConformer(0)
+    # conformer.GetPositions()
+    from openff.units import ensure_quantity
+
+    try:
+        positions = comp.to_openmm_positions()
+    except AttributeError:
+        positions = comp.to_openff().conformers[0]
+
+    return ensure_quantity(positions, "openmm")
+
+
+# TODO: This is probably something that should go in openmmtools/openmm
+def get_residue_index_from_atom_index(topology, atom_index):
+    """
+    Retrieve the residue index for a given atom index in an OpenMM topology.
+
+    This function iterates through the residues and their atoms in the topology
+    to locate the residue that contains the specified atom index.
+
+    Parameters
+    ----------
+    topology : openmm.app.Topology
+        The OpenMM topology object containing residues and atoms.
+    atom_index : int
+        The index of the atom whose residue ID is to be found.
+
+    Returns
+    -------
+    int
+        The index of the residue that contains the specified atom.
+
+    Raises
+    ------
+    ValueError
+        If the atom index is not found in the topology.
+    """
+    for residue in topology.residues():
+        for atom in residue.atoms():
+            if atom.index == atom_index:
+                return residue.index
+
+    # If the loop completes without finding the atom, raise the ValueError
+    raise ValueError(f"Atom index {atom_index} not found in topology.")
