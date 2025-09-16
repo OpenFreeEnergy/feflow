a9f5342b8e1faa51e2f9cbaba894cb0b8d6fa9af
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2025-09-16 18:39:50 -0400

    Actually skipping now... sigh

--- feflow/tests/test_protein_mutation.py
+++ feflow/tests/test_protein_mutation.py
@@ -576,7 +576,7 @@ class TestProtocolMutation:
             f"6 * dDDG ({6 * arg_lys_diff_error})"
         )
 
-    @pytest.skip(reason="Expected to fail so far, we are allowing proline mutations.")
+    @pytest.mark.skip(reason="Expected to fail so far, we are allowing proline mutations.")
     def test_proline_mutation_fails(
         self, ala_capped_system, pro_capped_system, ala_to_pro_mapping
     ):
