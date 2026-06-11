2d838fbf756d901de42524818bade15ce289ff49
ijpulidos <2949729+ijpulidos@users.noreply.github.com>
2026-06-11 18:21:43 -0400

    We only support MonteCarloBarostat for now

--- feflow/settings/integrators.py
+++ feflow/settings/integrators.py
@@ -35,7 +35,7 @@ class PeriodicNonequilibriumIntegratorSettings(SettingsBaseModel):
     """Number of steps for the equilibrium parts of the cycle. Default 12500"""
     nonequilibrium_steps: int = 12500
     """Number of steps for the non-equilibrium parts of the cycle. Default 12500"""
-    barostat: Literal["MonteCarloBarostat", "MonteCarloMembraneBarostat"] = (
+    barostat: Literal["MonteCarloBarostat"] = (
         "MonteCarloBarostat"
     )
     """
